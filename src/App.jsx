1d442a5828b5eed7632a4f933a7f39179cb63d08
parkercatz <33571737+parkercatz@users.noreply.github.com>
2021-08-10 18:47:19 +0900

    feat: useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>Hello World</h1>
       <ColorfulMessage color="blue">hoge</ColorfulMessage>
       <ColorfulMessage color="pink">fuga</ColorfulMessage>
-      <button onClick={onClickButton}>Button</button>
+      <button onClick={onClickCountUp}>CountUp</button>
+      <p>{num}</p>
     </>
   );
 };
