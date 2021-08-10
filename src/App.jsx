8d62cc530ee1de56d9217baf0a9709a35e48ec49
parkercatz <33571737+parkercatz@users.noreply.github.com>
2021-08-10 18:03:18 +0900

    feat: event & styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
   return (
     <>
-      <h1>Hello World</h1>
-      <p>hoge</p>
+      <h1 style={{ color: "red" }}>Hello World</h1>
+      <p style={contentStyle}>hoge</p>
+      <button onClick={onClickButton}>Button</button>
     </>
   );
 };
