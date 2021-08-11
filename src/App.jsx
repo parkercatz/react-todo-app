64bd0b7ebeba018dd0514b30ba9992e96030be77
parkercatz <33571737+parkercatz@users.noreply.github.com>
2021-08-11 15:55:03 +0900

    fix: reset file

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,6 @@
-/* eslint react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
-import { ColorfulMessage } from "./components/ColorfulMessage";
+import React from "react";
+import "./styles.css";
 
-const App = () => {
-  const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
-
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
-
-  const onClickSwitchShowFlag = () => {
-    setFaceShowFlag(!faceShowFlag);
-  };
-
-  useEffect(() => {
-    if (num > 0) {
-      if (num % 3 === 0) {
-        faceShowFlag || setFaceShowFlag(true);
-      } else {
-        faceShowFlag && setFaceShowFlag(false);
-      }
-    }
-  }, [num]);
-
-  return (
-    <>
-      <h1 style={{ color: "red" }}>Hello World</h1>
-      <ColorfulMessage color="blue">hoge</ColorfulMessage>
-      <ColorfulMessage color="pink">fuga</ColorfulMessage>
-      <button onClick={onClickCountUp}>CountUp</button>
-      <br />
-      <button onClick={onClickSwitchShowFlag}>on/off</button>
-      <p>{num}</p>
-      {faceShowFlag && <p>＼(^o^)／</p>}
-    </>
-  );
+export const App = () => {
+  return <div></div>;
 };
-
-export default App;
--- src/components/ColorfulMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ColorfulMessage = ({ color, children }) => {
-  const contentStyle = {
-    color,
-    fontSize: "18px"
-  };
-  return <p style={contentStyle}>{children}</p>;
-};
