00cc9ca6acadfc396edd803cdbdaf482f542cbee
parkercatz <33571737+parkercatz@users.noreply.github.com>
2021-08-11 15:50:01 +0900

    fix: default export → export

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const ColorfulMessage = ({ color, children }) => {
+export const ColorfulMessage = ({ color, children }) => {
   const contentStyle = {
     color,
     fontSize: "18px"
   };
   return <p style={contentStyle}>{children}</p>;
 };
-
-export default ColorfulMessage;
