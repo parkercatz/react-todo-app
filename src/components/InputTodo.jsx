0e726852e3701d19a2412c5d678ffd97828a15df
parkercatz <33571737+parkercatz@users.noreply.github.com>
2021-08-12 11:28:09 +0900

    fix: css in js

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 
+const style = {
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  backgroundColor: "#c1ffff",
+  borderRadius: "8px"
+};
+
 export const InputTodo = ({ todoText, onChange, onClick }) => {
   return (
-    <div className="input-area">
+    <div style={style}>
       <input value={todoText} onChange={onChange} placeholder="TODOを入力" />
       <button onClick={onClick}>追加</button>
     </div>
